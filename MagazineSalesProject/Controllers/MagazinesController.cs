0a4506940b2f73332ddf23c5b76d2ee11284d9aa
ChrisHelmsC <Chris.helms.c@gmail.com>
2015-11-26 15:50:47 -0800

    Initial Push
    Contains Database, Views and general webapp

---
+++ MagazineSalesProject/Controllers/MagazinesController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MagazineSalesProject.Models;
+
+namespace MagazineSalesProject.Controllers
+{
+    public class MagazinesController : Controller
+    {
+        MagazineDataEntities db = new MagazineDataEntities(); //Get a hold on DB
+
+        public ActionResult Index()
+        {
+            var magazines = from m in db.Magazines
+                            where m.MID > 2
+                            select m;
+
+            return View(magazines.ToList());
+        }
+    }
+}
---
+++ MagazineSalesProject/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MagazineSalesProject.MvcApplication" Language="C#" %>
